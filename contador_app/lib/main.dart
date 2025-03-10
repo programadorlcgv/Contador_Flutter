b1deb412b02b24a387ccfd01c29959c5d5efa341
programadorlcgv <programadorlcgv@gmail.com>
2025-03-10 09:42:56 -0500

    Funcionalidad añadida: la pantalla cambia y muestra el contador al presionar '+1

--- contador_app/lib/main.dart
+++ contador_app/lib/main.dart
@@ -12,6 +12,9 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner:  false,
+      theme: ThemeData(
+        colorSchemeSeed: Colors.blue
+      ),
       home: Scaffold(
         body: CounterScreen() ,
       )
--- contador_app/lib/presentation/screens/counter_screen.dart
+++ contador_app/lib/presentation/screens/counter_screen.dart
@@ -1,23 +1,41 @@
 import 'package:flutter/material.dart';
 
-class CounterScreen extends StatelessWidget {
+class CounterScreen extends StatefulWidget {
+
+
   const CounterScreen({super.key});
+
+  @override
+  State<CounterScreen> createState() => _CounterScreenState();
+}
+
+class _CounterScreenState extends State<CounterScreen> {
+
+  int clickCounter = 0;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Contador App'),),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            Text('10',style: TextStyle(fontSize: 100, fontWeight: FontWeight.w400),),
-            Text('10 clicks', style: TextStyle(fontSize: 20),)
+            Text('$clickCounter',style: TextStyle(fontSize: 100, fontWeight: FontWeight.w400),),
+            Text('$clickCounter click${clickCounter == 1 ? '' : 's'}', style: TextStyle(fontSize: 20),)
           ],
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {}, 
-        child: Icon(Icons.plus_one_outlined),),
+        onPressed: () {
+          setState(() {
+            clickCounter++;
+          });
+          
+        }, 
+        child: Icon(Icons.plus_one_outlined),
+      ),
     );  
   }
 }
